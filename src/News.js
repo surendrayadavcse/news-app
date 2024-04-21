677204012c2a609a48db80b53664473586e9034a
Nandyala Venkata Surendra <155749543+surendrayadavcse@users.noreply.github.com>
2024-04-21 20:14:26 +0530

    Update News.js

--- src/News.js
+++ src/News.js
@@ -6,14 +6,14 @@ export default function News(props){
 
         {
          props.article.urlToImage!==null?
-         <img className="img" src={props.article.urlToImage}></img>:
+         <img alt=img2 className="img" src={props.article.urlToImage}></img>:
         
-         <img src="https://t3.ftcdn.net/jpg/04/34/72/82/240_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg"></img>
+         <img src="https://t3.ftcdn.net/jpg/04/34/72/82/240_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg" alt="image1"></img>
         }
         
         </div>
         <h4 className="heading">{props.article.title}</h4>
-        <p>{props.article.description?.substring(0,60).concat("...")} <a href={props.article.url} target="_blank">Read More</a></p>
+        <p>{props.article.description?.substring(0,60).concat("...")} <a href={props.article.url} target="_blank" rel="noreferrer">Read More</a></p>
         
         <div className="source"> 
             <p>{props.article.author?.substring(0,6)}</p>
