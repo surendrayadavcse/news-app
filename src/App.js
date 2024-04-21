af2a4d6995109e692acc0c41f8b56937989a6705
Nandyala Venkata Surendra <155749543+surendrayadavcse@users.noreply.github.com>
2024-04-21 20:46:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,22 +9,26 @@ function App() {
 
   let [articles,setArticles]=useState([])
   let [category,setCategory]=useState("india");
- 
+  
   
   useEffect(()=>{
 
-       
-        fetch(`https://newsapi.org/v2/everything?q=${category}&from=2024-04-15&apiKey=3430e2c5c40d47be86181153c3424fdc`)
+     
+        
+         
+        fetch(`https://newsapi.org/v2/everything?q=${category}&apiKey=3430e2c5c40d47be86181153c3424fdc`)
         .then((response)=>response.json())//extract data
         .then((news)=>{
+          console.log(news)
+          
           setArticles(news.articles)
-          console.log(news.articles)
+          
         })
         .catch((err)=>{
           console.log(err)
         })
 
-  },[category])
+  })
 
 
 
@@ -43,10 +47,11 @@ function App() {
         }} placeholder='search'></input>
 
        
+       
         </header>
 
       <section className='news-articles'>
-      {
+      { 
         articles.map((article)=>{
           return(<News article={article}></News>)
         })
@@ -59,3 +64,5 @@ function App() {
 }
 
 export default App;
+
+
